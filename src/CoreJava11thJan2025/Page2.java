4a4ae67a50ab37b33d2b4f27417ef138d33bc0ba
VaseemAhamadSyed <vaseem.syed6949@gmail.com>
2025-01-11 10:10:06 +0530

    commit 2on 11th Jan 2025

---
+++ src/CoreJava11thJan2025/Page2.java
@@ -0,0 +1,8 @@
+package CoreJava11thJan2025;
+
+public class Page2 {
+    public static void main(String[] args) {
+        System.out.println("9*1=9");
+        System.out.println("9*2=18");
+    }
+}
