43d8f6615b6b9a3c8059ae03266b4917a2c01449
VaseemAhamadSyed <vaseem.syed6949@gmail.com>
2025-01-11 10:19:55 +0530

    commit 3rd 11th Jan 2025

---
+++ src/CoreJava11thJan2025/Page3.java
@@ -0,0 +1,8 @@
+package CoreJava11thJan2025;
+
+public class Page3 {
+    public static void main(String[] args) {
+        int age = 36;
+        System.out.printf("your age is %d",age);
+    }
+}
---
+++ src/CoreJava11thJan2025/Page4.java
@@ -0,0 +1,8 @@
+package CoreJava11thJan2025;
+
+public class Page4 {
+    public static void main(String[] args) {
+        float price = 4599.99f;
+        System.out.println("Item price is " + price);
+    }
+}
